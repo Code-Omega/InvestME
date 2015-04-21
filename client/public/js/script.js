2e48563ad97fb3a7d5563ca6950711172ce9e99d
Code-Omega <Clue@isr5884.urh.uiuc.edu>
2015-04-21 00:01:29 -0500

    client pushed

---
+++ client/public/js/script.js
@@ -0,0 +1,40 @@
+/*var divs = document.getElementsByClassName('alert');
+for(var i=0; i<divs.length; i++) {
+  divs[i].addEventListener("click", highlightThis);*/
+  /*
+  divs[i].addEventListener("click", highlightThis, true);
+  divs[i].addEventListener("click", highlightThis, false);*/
+//}
+
+/*function highlightThis(event) {
+    //event.stopPropagation();
+  
+    var backgroundColor = this.style.backgroundColor;
+    this.style.backgroundColor='yellow';
+    alert(this.className);
+    this.style.backgroundColor=backgroundColor;
+}*/
+
+$(document).foundation();
+/*$(document).ready(function(){
+    angular.module('app', [
+      '720kb.datepicker'
+      ]);
+};
+(function withAngular(angular) {
+  'use strict';
+
+  angular.module('720kb', [
+    'ngRoute',
+    '720kb.datepicker',
+    'hljs'
+  ]).config(['hljsServiceProvider', function configurationFunction(hljsServiceProvider) {
+
+    hljsServiceProvider.setOptions({
+      // replace tab with 4 spaces
+      'tabReplace': ''
+    });
+  }]);
+}(angular));*/
+
+
