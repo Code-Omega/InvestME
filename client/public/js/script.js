0431ca05df774222faabaa6682c0f105a5896517
FrownyFace <soham.waychal@gmail.com>
2015-05-09 23:12:40 -0700

    Mystery in all its life

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
   //$('#test').rssfeed('http://finance.yahoo.com/rss/headline?s=^GSPC,^dji,^ixic', {limit: 100, header:false, content:false, media:false, date:false});
-  $("#portbtn").click(function(){
-    $("#portfolio").fadeToggle("fast");
+  $("#portbtn").click(function(e){
+      $("#portfolio").fadeToggle("slow");
+      e.stopPropagation();
   });
+  $("#stockbtn").click(function(e){
+      $("#stockbar").fadeToggle("slow");
+      console.log("hithithithi");
+      e.stopPropagation();
+  });
+  $("body").click(function(){
+      $("#portfolio").fadeOut("slow");
+      $("#stockbar").fadeOut("slow");
+  });
+
 });
