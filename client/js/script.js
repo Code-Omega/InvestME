6b88dfc07f77c77b5af2e86e9afa9acad6118b50
Code-Omega <Clue@isr5884.urh.uiuc.edu>
2015-05-10 06:12:38 -0500

    more on add port

--- client/js/script.js
+++ client/js/script.js
@@ -10,8 +10,9 @@ $(document).ready(function () {
       e.stopPropagation();
   });
   $("body").click(function(e){
-    //console.log(e.target.nodeName);
-    $("#portfolio").fadeOut("slow");
+    console.log(e.target.nodeName);
+    if(e.target.nodeName!='INPUT')
+      $("#portfolio").fadeOut("slow");
     if(e.target.nodeName!='P')
       $("#stockbar").fadeOut("slow");
   });
