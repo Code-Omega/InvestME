78dd0e16bc286b01211a160464fbc6883f1faf34
Code-Omega <Clue@isr5884.urh.uiuc.edu>
2015-05-10 15:53:06 -0500

    setting page set up

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -8,7 +8,7 @@ demoApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   when('/', {
     templateUrl: 'partials/main.html',
-    controller: 'FeedController'
+    controller: 'FeedController',
   }).
   when('/portfolio', {
     templateUrl: 'partials/portfolio.html',
@@ -20,11 +20,28 @@ demoApp.config(['$routeProvider', function($routeProvider) {
   }).
   when('/register', {
     templateUrl: 'partials/login.html',
-    controller: 'RegisterController'
+    controller: 'RegisterController',
+    resolve:{
+        "check":function($window, $location){   
+            if(window.localStorage.length > 0){ 
+                if(window.localStorage.getItem("user") != undefined){
+                    if(window.localStorage.getItem("user") != "undefined"){
+                        $location.path('/setting');
+                    }
+                }
+            }else{
+                //not logged in yet
+            }
+        }
+    }
   }).
   when('/search', {
     templateUrl: 'partials/search.html',
     controller: 'SearchController'
+  }).
+  when('/setting', {
+    templateUrl: 'partials/setting.html',
+    controller: 'MainController'
   }).
   otherwise({
     redirectTo: '/'
